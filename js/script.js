a5f0f2676ec5704d374436dced9d247d016b607f
Artem R <a.rikov18079@gmail.com>
2025-09-24 10:49:02 +0200

    Add fixed header and animations for it

--- js/script.js
+++ js/script.js
@@ -1,18 +1,36 @@
-// JavaScript to change the background image on selection change
+// Background picker
+
 const backgroundPicker = document.getElementById('backgroundPicker');
 const body = document.body;
 
-// Event listener for when the user selects an option
 backgroundPicker.addEventListener('change', function() {
-    const selectedImage = backgroundPicker.value; // Get the selected value (image file name)
-    body.style.backgroundImage = `url('${selectedImage}')`; // Update the body background
-    body.style.backgroundSize = "cover"; // Ensure the background image covers the page
-    body.style.backgroundPosition = "center"; // Center the image
-    body.style.backgroundAttachment = "fixed"; // Keep the background fixed while scrolling
+    const selectedImage = backgroundPicker.value; 
+    body.style.backgroundImage = `url('${selectedImage}')`; 
+    body.style.backgroundSize = "cover"; 
+    body.style.backgroundPosition = "center"; 
+    body.style.backgroundAttachment = "fixed"; 
 });
 
-// Set default background image (optional)
 body.style.backgroundImage = "url('./img/skul-profile-backgrounds-rare.jpg')";
 body.style.backgroundSize = "cover";
 body.style.backgroundPosition = "center";
 body.style.backgroundAttachment = "fixed";
+
+// Header animation
+
+let lastScroll = 0;
+const header = document.querySelector(".header");
+
+setTimeout(() => {
+    window.addEventListener("scroll", () => {
+        const currentScrole = window.pageYOffset;
+
+        if (currentScrole > lastScroll && currentScrole > 100) {
+            header.classList.add("hide");
+        } else {
+            header.classList.remove("hide");
+        }
+
+        lastScroll = currentScrole;
+    });
+}, 700);
